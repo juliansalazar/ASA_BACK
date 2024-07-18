65505fa050be62cb04f38f407a15cb0b9b9d8d9d
unknown <juliansalazarvelez94@gmail.com>
2024-07-17 21:04:59 -0500

    Error fix server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 require('dotenv').config();
-const { errorHandler } = require('../back/middlewares/errorMiddleware');
+const { errorHandler } = require('./middlewares/errorMiddleware.js');
 const colors = require('colors');
-const connectDB = require('../back/config/db.js');
+const connectDB = require('./config/db.js');
 const cors = require('cors');
 
 const port = process.env.PORT || 8081;
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/placas', require('./routes/placasRoutes.js'));
-app.use('/api/users', require('../back/routes/usersRoutes.js'));
-app.use('/api/products', require('../back/routes/productsRoutes.js'));
+app.use('/api/users', require('./routes/usersRoutes.js'));
+app.use('/api/products', require('./routes/productsRoutes.js'));
 app.use(errorHandler);
 app.listen(port, () => console.log(`listening en ${port}`));
