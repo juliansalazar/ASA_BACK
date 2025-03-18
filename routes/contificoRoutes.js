c9b2836feede76dbd5e18362ac2e1fbdd849cf0a
unknown <juliansalazarvelez94@gmail.com>
2025-03-18 15:25:56 -0500

    Debugg

--- routes/contificoRoutes.js
+++ routes/contificoRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/client-id', async (req, res) => {
   const { identificacion, tipo } = req.query;
-  const apiKey = process.env.CONTIFICO_API_TOKEN;
+  const apiKey = process.env.CONTIFICO_API_KEY;
 
   if (!apiKey) {
     return res.status(500).json({ error: 'Clave API no definida' });
